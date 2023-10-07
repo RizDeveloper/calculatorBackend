fcf038d35b298df1e61b62072935e533f6203b06
developer001 <developer001@rizsolutions.com>
2023-10-07 19:47:29 +0500

    formatting the code

--- server.py
+++ server.py
@@ -5,11 +5,13 @@ from calculator import calculate
 app = Flask(__name__)
 CORS(app)  # Allow all origins
 
+
 @app.route('/api/calculate', methods=['POST'])
 def calculate_route():
     return calculate(request)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
-    #done
+    # done
